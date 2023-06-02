927c3e28234b31ef498de55c45998f2d7ca92ead
TheNameless31 <58726827+TheNameLess31@users.noreply.github.com>
2023-06-02 23:13:44 +0200

    No changes in particular

--- UnityTut/Assets/Player.cs
+++ UnityTut/Assets/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
     private float horizontalInput;
     private Rigidbody rigidbodyComponent;
     private int superJumpsRemaining;
+  
 
     // Start is called before the first frame update
     void Start()
