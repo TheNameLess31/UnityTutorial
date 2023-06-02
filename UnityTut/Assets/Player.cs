66bb40ea3066b226f8c87d292da5f6ffd826bd2d
TheNameless31 <max.konieczka@gmx.de>
2023-06-02 13:39:21 +0200

    Added moving, jumping and additional tile blocks

--- UnityTut/Assets/Player.cs
+++ UnityTut/Assets/Player.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+
     private bool jumpKeyWasPressed;
     private float horizontalInput;
+    private Rigidbody rigidbodyComponent;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbodyComponent = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,15 +24,25 @@ public class Player : MonoBehaviour
         {
             jumpKeyWasPressed = true;
         }
+
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 
     // FixedUpdate is called once every physics update
     private void FixedUpdate()
     {
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0);
+
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+        {
+            return;
+        }
+
         if (jumpKeyWasPressed)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            rigidbodyComponent.AddForce(Vector3.up * 6, ForceMode.VelocityChange);
             jumpKeyWasPressed = false;
         }
+
     }
 }
